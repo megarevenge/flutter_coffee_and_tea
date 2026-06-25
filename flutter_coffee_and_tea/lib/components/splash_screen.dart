5ecaef44344e4b274179ffe83c9567b1d8994c6f
Jaxongir <132773984+megarevenge@users.noreply.github.com>
2026-06-25 22:53:30 +0500

    changed icon

--- flutter_coffee_and_tea/lib/components/splash_screen.dart
+++ flutter_coffee_and_tea/lib/components/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 7), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
